0d505e5f8f1f028ad7829b3498c2f47bfc66cf45
Vladislav <sagovvladus@gmail.com>
2025-11-23 23:53:19 +0500

    Return ControlDigits files

---
+++ cs/ControlDigit/Snils/SnilsExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ControlDigit
+{
+    public static class SnilsExtensions
+    {
+        public static int CalculateSnils(this long number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
